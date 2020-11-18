22460ea66adb572e15e9c55a4b9d5f500be6141d
manosh7n <manoshin.dm@gmail.com>
2020-11-18 20:24:53 +1000

    refactoring

--- app.py
+++ app.py
@@ -3,35 +3,40 @@ import pickle
 import numpy as np
 import mediapipe as mp
 from scipy.stats import mode
-from classifier.classifier import Classifier
+from cnn_clf.classifier import Classifier
 
 
 def show_predict(predict, frame, pos, clf_name):
+    """
+    Shows the prediction at the position (pos) in the frame
+
+        Window
+    |0         1|
+    |          2|
+    |           |
+    """
     if pos == 0:
-        shift = frame.shape[1]-80
+        shift_x = 5
         shift_y = 38
-        cv2.rectangle(frame, (shift-5, 22), (frame.shape[1]-3, 5), (255, 255, 255), thickness=-1)
-        cv2.putText(frame, clf_name,
-                    (shift, 20), cv2.FONT_HERSHEY_COMPLEX, 0.47,
-                    (0, 0, 0), 2)
+        cv2.rectangle(frame, (shift_x, shift_y - 15), (75, shift_y - 30),
+                      (255, 255, 255), thickness=-1)
     elif pos == 1:
-        shift = 5
+        shift_x = frame.shape[1] - 80
         shift_y = 38
-        cv2.rectangle(frame, (shift, 22), (75, 5), (255, 255, 255), thickness=-1)
-        cv2.putText(frame, clf_name,
-                    (shift, 20), cv2.FONT_HERSHEY_COMPLEX, 0.47,
-                    (0, 0, 0), 2)
+        cv2.rectangle(frame, (shift_x - 5, shift_y - 15), (frame.shape[1] - 3, shift_y - 30),
+                      (255, 255, 255), thickness=-1)
     else:
-        shift = frame.shape[1] - 80
+        shift_x = frame.shape[1] - 80
         shift_y = 235
-        cv2.rectangle(frame, (shift - 5, 219), (frame.shape[1] - 3, 205), (255, 255, 255), thickness=-1)
-        cv2.putText(frame, clf_name,
-                    (shift, 217), cv2.FONT_HERSHEY_COMPLEX, 0.47,
-                    (0, 0, 0), 2)
+        cv2.rectangle(frame, (shift_x - 5, shift_y - 15), (frame.shape[1] - 3, shift_y - 30),
+                      (255, 255, 255), thickness=-1)
+
+    cv2.putText(frame, clf_name, (shift_x, shift_y - 18),
+                cv2.FONT_HERSHEY_COMPLEX, 0.47, (0, 0, 0), 2)
     pred_sign = np.argmax(predict)
     for i, sign in enumerate(ALPH):
         cv2.putText(frame, f'{sign}: {predict[i]:.2f}',
-                    (shift, shift_y + (20*i+1)), cv2.FONT_HERSHEY_COMPLEX, 0.62,
+                    (shift_x, shift_y + (20 * i + 1)), cv2.FONT_HERSHEY_COMPLEX, 0.62,
                     (50, 224, 30) if pred_sign == i else (20, 20, 230), 2)
 
 
@@ -65,22 +70,23 @@ def draw_progress_bar(frame, frame_count, word):
 
 
 WINDOW = "Gesture recognition"
-CLASSIFIER_MODEL_PATH = "models/model_mobile9.h5"
+CNN_CLASSIFIER_PATH = "models/model_mobile9.h5"
 KEY_POINTS_CLASSIFIER_PATH = 'models/lr_model.sav'
-DEVICE_ID = 0
+DEVICE_ID = 1
 ALPH = ['А', 'Б', 'В', 'Г', 'И', 'К', 'Н', 'О', 'С']
 
-fourcc = cv2.VideoWriter_fourcc(*"MJPG")
-writer = cv2.VideoWriter('videos/output_01.avi', fourcc, 10, (640, 480), True)
-classifier = Classifier(CLASSIFIER_MODEL_PATH)
-loaded_model = pickle.load(open(KEY_POINTS_CLASSIFIER_PATH, 'rb'))
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(
-    max_num_hands=1,
-    min_detection_confidence=0.5, min_tracking_confidence=0.5)
 cap = cv2.VideoCapture(DEVICE_ID)
 cv2.namedWindow(WINDOW)
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+writer = cv2.VideoWriter('videos/output_01.avi', fourcc, 10, (640, 480), True)
+
+cnn_clf = Classifier(CNN_CLASSIFIER_PATH)
+kp_clf = pickle.load(open(KEY_POINTS_CLASSIFIER_PATH, 'rb'))
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(max_num_hands=1,
+                       min_detection_confidence=0.5,
+                       min_tracking_confidence=0.5)
 
 frame_count = 0
 predictions = []
@@ -97,33 +103,34 @@ while cap.isOpened():
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+    # If there is a hand in the frame
     if results.multi_hand_landmarks:
         frame_count += 1
-        x_coo = []
-        y_coo = []
-        pts_to_pred = []
+        x_coords, y_coords, points_xyz = [], [], []
+        hand_landmarks = results.multi_hand_landmarks[0]
 
-        for hand_landmarks in results.multi_hand_landmarks:
-            for mark in hand_landmarks.landmark:
-                pts_to_pred.extend([mark.x, mark.y, mark.z])
-                x_coo.append(mark.x)
-                y_coo.append(mark.y)
+        for mark in hand_landmarks.landmark:
+            points_xyz.extend([mark.x, mark.y, mark.z])
+            x_coords.append(mark.x)
+            y_coords.append(mark.y)
 
-            crop = draw_crop_bb(image, x_coo, y_coo)
-            mp_drawing.draw_landmarks(
-                image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+        crop = draw_crop_bb(image, x_coords, y_coords)
+        mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-            pts_to_pred = np.array(pts_to_pred).reshape(1, -1)
-            pred_pts = loaded_model.predict_proba(pts_to_pred)[0]
-            pred_cnn = classifier(crop)[0]
-            mean_pred = (pred_cnn*0.4 + pred_pts*1.6) / 2
-            predictions.append(np.argmax(mean_pred))
+        points_xyz = np.array(points_xyz).reshape(1, -1)
+        pred_pts = kp_clf.predict_proba(points_xyz)[0]
+        pred_cnn = cnn_clf(crop)[0]
+        mean_pred = (pred_cnn * 0.4 + pred_pts * 1.6) / 2
+        predictions.append(np.argmax(mean_pred))
 
-            # show_predict(pred_pts, image, 0, '  Points')
-            # show_predict(pred_cnn, image, 1, ' CNN')
-            show_predict(mean_pred, image, 1, '  Predict')
+        show_predict(pred_pts, image, 0, '  Points')
+        show_predict(pred_cnn, image, 1, '  CNN')
+        show_predict(mean_pred, image, 2, '  Predict')
 
     draw_progress_bar(image, frame_count, word)
+    cv2.imshow(WINDOW, image)
+
+    # Take the most frequently predicted gesture within 30 frames
     if frame_count == 30:
         mode_predict, val_count = mode(predictions)
         if val_count > frame_count // 2:
@@ -133,10 +140,10 @@ while cap.isOpened():
     if writer is not None:
         writer.write(image)
 
-    cv2.imshow(WINDOW, image)
     key = cv2.waitKey(5)
     if key & 0xFF == 27:
         break
+    # Clear the progress bar and word
     if key == ord('c') or len(word) >= 45:
         word = ''
         frame_count = 0
