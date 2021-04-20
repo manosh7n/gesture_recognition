8ec7217ea7f1fe49b2db455f4dfdb6738270c986
manosh7n <manoshin.dm@gmail.com>
2021-04-20 23:12:56 +1000

    refactoring

--- App.py
+++ App.py
@@ -7,33 +7,61 @@ from utils.GlobalVar import *
 from more_itertools import unique_justseen as uj
 
 
-def show_predict(predict, frame):
-    shift_x = 5
-    shift_y = 38
-    cv2.rectangle(frame, (shift_x, shift_y - 15), (75, shift_y - 30),
-                  (255, 255, 255), thickness=-1)
-
-    cv2.putText(frame, 'Алфавит', (shift_x, shift_y - 18),
-                cv2.FONT_HERSHEY_COMPLEX, 0.47, (0, 0, 0), 2)
-    pred_sign = np.argmax(predict)
-    for i, sign in enumerate(classes):
-        cv2.putText(frame, f'{sign}: {predict[i]:.2f}',
-                    (shift_x, shift_y + (20 * i + 1)), cv2.FONT_HERSHEY_COMPLEX, 0.62,
-                    (50, 224, 30) if pred_sign == i else (20, 20, 230), 2)
-
-
 cap = cv2.VideoCapture(DEVICE_ID)
 cv2.namedWindow(WINDOW)
+clf = pickle.load(open(KEY_POINTS_CLASSIFIER_PATH, 'rb'))
 # fourcc = cv2.VideoWriter_fourcc(*"MJPG")
 # writer = cv2.VideoWriter('videos/output_01.avi', fourcc, 10, (1280, 720), True)
-
-clf = pickle.load(open(KEY_POINTS_CLASSIFIER_PATH, 'rb'))
 
 frame_count = 0
 predictions = []
 prev_predictions = []
 isRecording = False
 prev_length = 0
+
+
+def show_predict(predict: np.ndarray[float], frame: np.ndarray[float], corner_coo: tuple[int, int]):
+    """
+    Displaying the classified gesture in the upper-right corner of the bounding box
+    @param predict: array of predicted probabilities
+    @param frame: the frame where the letter is displayed
+    @param corner_coo: the coordinate where the letter is displayed
+    """
+    cv2.rectangle(frame, corner_coo, (corner_coo[0] + 70, corner_coo[1] - 70),
+                  (255, 255, 255), thickness=-1)
+    cv2.putText(frame, f'{classes[np.argmax(predict)]}', (corner_coo[0] + 18, corner_coo[1] - 18),
+                cv2.FONT_HERSHEY_COMPLEX, 1.7, (0, 0, 0), 2)
+
+
+def draw_bb(points: np.ndarray[float]) -> (float, tuple[int, int]):
+    """
+        Draw bounding box and and returns the difference between the lengths of the diagonals between the frames
+        (it is necessary to check whether the gesture changes or the same one is displayed), the coordinates
+        of the upper-right corner of the bounding box for showing the prediction
+    @param points: (x, y) coordinates or landmarks
+    @return:
+           percent_diff: percentage difference between the diagonal from the last frame and the current one
+           right_up_corner: coordinate of right up corner
+    """
+    global prev_length
+    x_max, x_min = int(np.max(points[0][::2]) * image.shape[1]), int(np.min(points[0][::2]) * image.shape[1])
+    y_max, y_min = int(np.max(points[0][1::2]) * image.shape[0]), int(np.min(points[0][1::2]) * image.shape[0])
+    right_up_corner = (x_max, y_min)
+    line_length = np.sqrt((x_max - x_min) ** 2 + (y_max - y_min) ** 2)
+    diff = np.abs(line_length - prev_length)
+    prev_length = line_length
+    percent_diff = diff * 100 / line_length
+
+    cv2.line(image, (x_max, y_min), (x_min, y_min), (200, 10, 10), 2, cv2.LINE_AA)
+    cv2.line(image, (x_min, y_min), (x_max, y_max), (200, 10, 10), 2, cv2.LINE_AA)
+    cv2.line(image, (x_max, y_min), (x_max, y_max), (200, 10, 10), 2, cv2.LINE_AA)
+    cv2.line(image, (x_max, y_max), (x_min, y_max), (200, 10, 10), 2, cv2.LINE_AA)
+    cv2.line(image, (x_min, y_min), (x_min, y_max), (200, 10, 10), 2, cv2.LINE_AA)
+    cv2.circle(image, (x_max, y_max), 2, (10, 10, 220), 3)
+    cv2.circle(image, (x_min, y_min), 2, (10, 10, 220), 3)
+
+    return percent_diff, right_up_corner
+
 
 while cap.isOpened():
     success, image = cap.read()
@@ -58,22 +86,13 @@ while cap.isOpened():
             points_xyz.extend([mark.x, mark.y])
 
         points_xyz = np.array(points_xyz).reshape(1, -1)
-
-        x_max, x_min = int(np.max(points_xyz[0][::2]) * image.shape[1]), int(np.min(points_xyz[0][::2]) * image.shape[1])
-        y_max, y_min = int(np.max(points_xyz[0][1::2]) * image.shape[0]), int(np.min(points_xyz[0][1::2]) * image.shape[0])
-        line_length = np.sqrt((x_max - x_min)**2 + (y_max - y_min)**2)
-        diff = np.abs(line_length - prev_length)
-        prev_length = line_length
-        percent_diff = diff * 100 / line_length
-        cv2.line(image, (x_max, y_max), (x_min, y_min), (200, 10, 10), 2, cv2.LINE_AA)
-        cv2.circle(image, (x_max, y_max), 1, (10, 10, 220), 3)
-        cv2.circle(image, (x_min, y_min), 1, (10, 10, 220), 3)
+        difference, right_up_corner = draw_bb(points_xyz)
 
         pred = clf.predict_proba(points_xyz)[0]
-        show_predict(pred, image)
+        show_predict(pred, image, right_up_corner)
 
         if isRecording:
-            if np.max(pred) > 0.99 and percent_diff < 0.75:
+            if np.max(pred) > 0.99 and difference < 0.75 and frame_count % 2 == 0:
                 if len(prev_predictions) > 10:
                     letter = mode(prev_predictions)[0][0]
                     if len(predictions) == 0 or predictions[-1] != letter:
@@ -84,8 +103,12 @@ while cap.isOpened():
     cv2.imshow(WINDOW, image)
 
     key = cv2.waitKey(5)
+
     if key & 0xFF == 27 or key == ord('q'):
         break
+
+    # if "s" is pressed, the predicted letters are written to the list,
+    # and then the closest word to the word in the list is calculated using the Levenshtein distance.
     if key == ord('s'):
         if isRecording:
             isRecording = False
